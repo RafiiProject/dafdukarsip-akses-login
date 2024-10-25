9ccee89645119182736f2dde1b66f807c76dcfb6
RafiiProject <mzrafi1708@gmail.com>
2024-10-25 07:25:02 +0700

    update lagi ok

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,17 @@ import "./style/dark.scss";
 import {useContext} from "react";
 import {DarkModeContext} from "./context/darkModeContext";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { AuthContext } from "./context/AuthContext";
+
 
 function App() {
   const {darkMode} = useContext(DarkModeContext);
-
-  const { currentUser } = useContext(AuthContext)
-
-  const RequireAuth = ({ children }) => {
-    return currentUser ? children : <Navigate to="/login" />;
-  };
-  
-  const NotRequireAuth = ({ children }) => {
-    return currentUser ? <Navigate to="/" /> : children;
-  };
 
   return (
     <div className={darkMode ? "app dark" : "app"}>
       <BrowserRouter>
         <Routes>
           <Route path="/">
-            <Route index element={<RequireAuth><Home /></RequireAuth>}></Route>
+            <Route index element={<Home />}></Route>
           </Route>
         </Routes>
       </BrowserRouter>
